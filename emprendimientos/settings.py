489add513b955dfa7db6f11c24aba95d82024eff
Marlon <marsantovi@gmail.com>
2022-08-04 13:17:28 -0500

    Database config

--- emprendimientos/settings.py
+++ emprendimientos/settings.py
@@ -188,8 +188,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 
 # agreagdo recien
