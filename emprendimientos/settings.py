0384a5f268f5b277363e03f3380924e85a3185e9
Marlon <marsantovi@gmail.com>
2022-08-04 13:57:27 -0500

    Database config

--- emprendimientos/settings.py
+++ emprendimientos/settings.py
@@ -191,7 +191,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
