c5b6a3daefdf1cfd5928ce8d4ff79aef903b3435
Marlon <marlon_santo@hotmail.com>
2022-08-05 09:56:19 -0500

    Update settings.py

--- emprendimientos/settings.py
+++ emprendimientos/settings.py
@@ -37,7 +37,6 @@ IS_HEROKU = "DYNO" in os.environ
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'z=$5rf4rm+$lawj#(#2mpw36vh8#)9!hwl!^_(m%6d#z=1@wj%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
